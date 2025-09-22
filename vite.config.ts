19a5f3a421f78d66aa7f2a8198105caa9dfc523a
Imam Satrio <imamsatrio357@gmail.com>
2025-09-22 07:42:07 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
+  base: '/notaris-rina/',
   server: {
     host: "::",
     port: 8080,
